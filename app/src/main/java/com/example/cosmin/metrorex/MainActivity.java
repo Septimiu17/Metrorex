d6a74f1bac54fe55680ee51d587b3b4c30d13108
cicumihai <cicu.mihai@yahoo.com>
2017-06-07 18:31:20 +0300

    Inregistrarea cu Firebase

---
+++ app/src/main/java/com/example/cosmin/metrorex/MainActivity.java
@@ -0,0 +1,161 @@
+package com.example.cosmin.metrorex;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.cosmin.metrorex.Model.UserInformation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonRegister;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+    private TextView textViewSignIn;
+
+    private ProgressDialog progressDialog;
+
+    private FirebaseAuth firebaseAuth;
+
+    private DatabaseReference databaseReference;
+    private StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+
+
+        storageReference=FirebaseStorage.getInstance().getReference();
+
+
+        progressDialog=new ProgressDialog(this);
+
+        buttonRegister=(Button)findViewById(R.id.buttonRegister);
+
+        editTextEmail=(EditText)findViewById(R.id.etusername);
+        editTextPassword=(EditText)findViewById(R.id.etpassword);
+
+        textViewSignIn=(TextView)findViewById(R.id.tvsignin);
+
+        buttonRegister.setOnClickListener(this);
+        textViewSignIn.setOnClickListener(this);
+
+
+
+
+
+    }
+
+    private void registerUser() {
+        String email=editTextEmail.getText().toString().trim();
+        String password=editTextPassword.getText().toString().trim();
+
+        if(TextUtils.isEmpty(email)) {
+            //email is empty
+            Toast.makeText(this,"Please enter an email address", Toast.LENGTH_SHORT).show();
+            //stop the function to progress
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)) {
+            //password is empty
+            Toast.makeText(this,"Please enter a password",Toast.LENGTH_SHORT).show();
+            //stop the function to progress
+            return;
+        }
+
+        //if validations are ok show progresDialog
+        progressDialog.setMessage("Registering user.");
+        progressDialog.show();
+
+
+
+        firebaseAuth.createUserWithEmailAndPassword(email,password).
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //user is succesfully registered and logged in
+                            //start the profile activty
+                            Toast.makeText(MainActivity.this, "Registered Succesfully !", Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                            UserInformation userInformation = new UserInformation();
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            databaseReference.child(user.getUid()).setValue(userInformation);
+
+                            
+                            } catch (WriterException e) {
+                                e.printStackTrace();
+                            }
+
+                            startActivity(new Intent(MainActivity.this, Login.class));
+                        } else {
+                            Toast.makeText(MainActivity.this, "Could not register, please try again .", Toast.LENGTH_SHORT).show();
+                        }
+                        progressDialog.dismiss();
+                    }
+                });
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view == buttonRegister) {
+            registerUser();
+        }
+        if(view == textViewSignIn) {
+        //will open login activty here
+            startActivity(new Intent(this,Login.class));
+        }
+
+    }
+
+
+}
+
+
+
